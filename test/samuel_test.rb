fb11819b8331994d9886483415c39647197a06ae
Chris Kampmeier <chris@kampers.net>
2009-09-07 03:29:38 -0700

    Tests!

--- test/samuel_test.rb
+++ test/samuel_test.rb
@@ -2,22 +2,52 @@ require 'test_helper'
 
 class SamuelTest < Test::Unit::TestCase
 
-  context "Samuel" do
+  context "making an HTTP request" do
     setup do
-      Samuel.logger = nil # reset logger
+      setup_test_logger
+      FakeWeb.register_uri(:get, "http://example.com/test", :status => [200, "OK"])
+      Benchmark.stubs(:realtime).yields.returns(0.053)
+      Net::HTTP.start("example.com") { |query| query.get("/test") }
     end
 
-    should "have a logger" do
-      Samuel.logger
+    teardown { teardown_test_logger }
+
+    should_log_lines     1
+    should_log_including "HTTP request"
+    should_log_including "(53ms)"
+    should_log_including "[200 OK]"
+    should_log_including "GET http://example.com/test"
+
+
+    context "on a non-standard port" do
+      setup do
+        FakeWeb.register_uri(:get, "http://example.com:8080/test", :status => [200, "OK"])
+        Net::HTTP.start("example.com", 8080) { |query| query.get("/test") }
+      end
+
+      should_log_including "GET http://example.com:8080/test"
     end
 
-    should "be able to have its logger changed" do
-      Samuel.logger = :mock
-      assert_equal :mock, Samuel.logger
+    context "with SSL" do
+      setup do
+        FakeWeb.register_uri(:get, "https://example.com/test", :status => [200, "OK"])
+        http = Net::HTTP.new("example.com", 443); http.use_ssl = true
+        http.get("/test")
+      end
+
+      should_log_including "HTTP request"
+      should_log_including "GET https://example.com/test"
     end
 
-    should "log a basic HTTP request" do
-      Net::HTTP.start("example.com") { |query| query.get('/test') }
+    context "with SSL on a non-standard port" do
+      setup do
+        FakeWeb.register_uri(:get, "https://example.com:80/test", :status => [200, "OK"])
+        http = Net::HTTP.new("example.com", 80); http.use_ssl = true
+        http.get("/test")
+      end
+
+      should_log_including "HTTP request"
+      should_log_including "GET https://example.com:80/test"
     end
   end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,43 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'mocha'
+require 'fakeweb'
+
+FakeWeb.allow_net_connect = false
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'samuel'
 
 class Test::Unit::TestCase
+  TEST_LOG_PATH = File.join(File.dirname(__FILE__), 'test.log')
+
+  def self.should_log_lines(expected_count)
+    should "log #{expected_count} line#{'s' unless expected_count == 1}" do
+      lines = File.readlines(TEST_LOG_PATH)
+      assert_equal expected_count, lines.length
+    end
+  end
+
+  def self.should_log_including(what)
+    should "log a line including #{what.inspect}" do
+      contents = File.read(TEST_LOG_PATH)
+      if what.is_a?(Regexp)
+        assert_match what, contents
+      else
+        assert contents.include?(what)
+      end
+    end
+  end
+
+  def setup_test_logger
+    FileUtils.rm_rf TEST_LOG_PATH
+    FileUtils.touch TEST_LOG_PATH
+    Samuel.logger = Logger.new(TEST_LOG_PATH)
+  end
+
+  def teardown_test_logger
+    FileUtils.rm_rf TEST_LOG_PATH
+  end
 end
