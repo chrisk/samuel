a94552c235f78c84de2960b68ec581f5fe5984bc
Chris Kampmeier <chris@kampers.net>
2009-09-08 03:28:47 -0700

    Hardcore extract-method refactoring action

--- lib/samuel/request.rb
+++ lib/samuel/request.rb
@@ -1,38 +1,14 @@
 module Samuel
   class Request
+
     attr_accessor :response
 
     def initialize(http, request, proc)
       @http, @request, @proc = http, request, proc
-      @response = nil
     end
 
     def log!
-      milliseconds = (@seconds * 1000).round
-      if @http.use_ssl?
-        scheme = "https"
-        port   = (@http.port == 443) ? "" : ":#{@http.port}"
-      else
-        scheme = "http"
-        port   = (@http.port == 80) ? "" : ":#{@http.port}"
-      end
-      uri = "#{scheme}://#{@http.address}#{port}#{@request.path}"
-      method = @request.method.to_s.upcase
-
-      if response.is_a?(Exception)
-        response_info = response.class
-      else
-        response_info = "[#{response.code} #{response.message}]"
-      end
-
-      if [Exception, Net::HTTPClientError, Net::HTTPServerError].any? { |c| response.is_a?(c) }
-        level = Logger::WARN
-      else
-        level = Logger::INFO
-      end
-
-      bold_blue_on, reset = "\e[4;34;1m", "\e[0m"
-      Samuel.logger.add(level, "  #{bold_blue_on}HTTP request (#{milliseconds}ms) #{response_info}#{reset}  #{method} #{uri}")
+      Samuel.logger.add(log_level, log_message)
     end
 
     def execute!
@@ -41,6 +17,60 @@ module Samuel
       # for raising after the benchmark (and logging) is done.
       @seconds = Benchmark.realtime do
         begin; @response = @proc.call; rescue Exception => @response; end
+      end
+    end
+
+
+    private
+
+    def log_message
+      bold      = "\e[1m"
+      blue      = "\e[34m"
+      underline = "\e[4m"
+      reset     = "\e[0m"
+      "  #{bold}#{blue}#{underline}HTTP request (#{milliseconds}ms) " +
+      "#{response_summary}#{reset}  #{method} #{uri}"
+    end
+
+    def milliseconds
+      (@seconds * 1000).round
+    end
+
+    def uri
+      "#{scheme}://#{@http.address}#{port_if_not_default}#{@request.path}"
+    end
+
+    def scheme
+      @http.use_ssl? ? "https" : "http"
+    end
+
+    def port_if_not_default
+      ssl, port = @http.use_ssl?, @http.port
+      if (!ssl && port == 80) || (ssl && port == 443)
+        ""
+      else
+        ":#{port}"
+      end
+    end
+
+    def method
+      @request.method.to_s.upcase
+    end
+
+    def response_summary
+      if response.is_a?(Exception)
+        response.class
+      else
+        "[#{response.code} #{response.message}]"
+      end
+    end
+
+    def log_level
+      error_classes = [Exception, Net::HTTPClientError, Net::HTTPServerError]
+      if error_classes.any? { |klass| response.is_a?(klass) }
+        level = Logger::WARN
+      else
+        level = Logger::INFO
       end
     end
 
