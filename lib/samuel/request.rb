f80682168537128f0d4791c5bdca08b86e4a0e74
Chris Kampmeier <chris@kampers.net>
2009-09-08 02:51:27 -0700

    Move code out of the base module

---
+++ lib/samuel/request.rb
@@ -0,0 +1,46 @@
+module Samuel
+  class Request
+
+    def initialize(http, request, &block)
+      response, seconds = nil, 0
+
+      # If an exception is raised in this Benchmark block, it'll interrupt the
+      # benchmark. Instead, use an inner block to record it as the "response"
+      # for raising after the benchmark is done.
+      seconds = Benchmark.realtime do
+        begin; response = yield block; rescue Exception => response; end
+      end
+      raise response if response.is_a?(Exception)
+    ensure
+      milliseconds = (seconds * 1000).round
+      if http.use_ssl?
+        scheme = "https"
+        port   = (http.port == 443) ? "" : ":#{http.port}"
+      else
+        scheme = "http"
+        port   = (http.port == 80) ? "" : ":#{http.port}"
+      end
+      uri = "#{scheme}://#{http.address}#{port}#{request.path}"
+
+      method = request.method.to_s.upcase
+
+      if response.is_a?(Exception)
+        response_info = response.class
+      else
+        response_info = "[#{response.code} #{response.message}]"
+      end
+
+      if [Exception, Net::HTTPClientError, Net::HTTPServerError].any? { |c| response.is_a?(c) }
+        level = Logger::WARN
+      else
+        level = Logger::INFO
+      end
+
+      bold_blue_on, reset = "\e[4;34;1m", "\e[0m"
+      Samuel.logger.add(level, "  #{bold_blue_on}HTTP request (#{milliseconds}ms) #{response_info}#{reset}  #{method} #{uri}")
+
+      response
+    end
+
+  end
+end
