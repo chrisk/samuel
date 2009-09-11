f50fdc499c4637f1ce05b239ab02ff06be89005b
Chris Kampmeier <chris@kampers.net>
2009-09-11 02:58:31 -0700

    Add tests for default logger configuration

--- test/samuel_test.rb
+++ test/samuel_test.rb
@@ -2,6 +2,35 @@ require 'test_helper'
 
 class SamuelTest < Test::Unit::TestCase
 
+  context "logger configuration" do
+    setup do
+      Samuel.logger = nil
+      if Object.const_defined?(:RAILS_DEFAULT_LOGGER)
+        Object.send(:remove_const, :RAILS_DEFAULT_LOGGER)
+      end
+    end
+
+    teardown do
+      Samuel.logger = nil
+    end
+
+    context "when Rails's logger is available" do
+      setup { Object.const_set(:RAILS_DEFAULT_LOGGER, :mock_logger) }
+
+      should "use the same logger" do
+        assert_equal :mock_logger, Samuel.logger
+      end
+    end
+
+    context "when Rails's logger is not available" do
+      should "use a new Logger instance pointed to STDOUT" do
+        assert_instance_of Logger, Samuel.logger
+        assert_equal STDOUT, Samuel.logger.instance_variable_get(:"@logdev").dev
+      end
+    end
+  end
+
+
   context "making an HTTP request" do
     setup    { setup_test_logger
                FakeWeb.clean_registry }
