f1e6283736a4d09cd072987451a05f40edb89f83
Chris Kampmeier <chris@kampers.net>
2009-09-11 01:14:07 -0700

    Oops, make sure to clear the FakeWeb registry for each test

--- test/samuel_test.rb
+++ test/samuel_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class SamuelTest < Test::Unit::TestCase
 
   context "making an HTTP request" do
-    setup    { setup_test_logger }
+    setup    { setup_test_logger
+               FakeWeb.clean_registry }
     teardown { teardown_test_logger }
 
     context "to GET http://example.com/test, responding with a 200 in 53ms" do
